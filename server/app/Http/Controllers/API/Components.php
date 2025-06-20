e73348ae4582f396c19829e035d90605527b538b
naquinem <marlonagba@gmail.com>
2025-06-20 11:30:58 +0800

    add component controller

--- server/app/Http/Controllers/API/Components.php
+++ server/app/Http/Controllers/API/Components.php
@@ -7,5 +7,22 @@ use Illuminate\Http\Request;
 
 class Components extends Controller
 {
-    //
+    public function store(Request $request){
+        $validated = $request->validate([
+            'item_code' => 'required|string|max:255',
+            'product_name' => 'required|string|max:255',
+            'part' => 'required|string|max:255',
+            'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+            'price' => 'required|numeric',
+        ]);
+        $imagePath = $request->file('image')->store('images', 'public');
+        $component = Component::create([
+            'item_code' => $validated['item_code'],
+            'product_name' => $validated['product_name'],
+            'part' => $validated['part'],
+            'image' => $imagePath,
+            'price' => $validated['price'],
+            'quantity' => $validated['quantity']
+        ]);
+    }
 }
